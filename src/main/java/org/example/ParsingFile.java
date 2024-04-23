4a5823e289e716a131f3d72627c833a2918860c1
67134 <gigalskiy@gmail.com>
2024-04-23 21:55:39 +0300

    next 23.04.2024

--- src/main/java/org/example/ParsingFile.java
+++ src/main/java/org/example/ParsingFile.java
@@ -9,25 +9,28 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 public class ParsingFile {
 
     public static void readFile(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner sc = new Scanner(file);
-        String PATTERN_1 = "([А-Я]?[а-я*\\(?а-я*\\)?\\-?а-я*]*\\,*\\s)+([A-z])*([А-Я]?[а-я*\\(?а-я*\\)?\\-?а-я*]*\\,*\\s*)*";
+
+        String PATTERN_1 = "([А-Я]?[а-я*\\(?а-я*\\)?\\-?а-я*]*\\,*\\s)+([A-z\\-])*(\\d+)*([А-Я]?[а-я*\\(?а-я*\\)?\\-?а-я*]*\\.*\\,*\\s*)*";
         String PATTERN_2 = "([А-Я]{2,}\\,*\\s*)+";
-        String PATTERN_3 = "[A-Z]{1}\\d{1}\\.\\d{4}";
+        String PATTERN_3 = "[A-Z]{1}\\d{1,2}\\.\\d{4}";
         int i = 0;
         Pattern pattern1 = Pattern.compile(PATTERN_1);
         Pattern pattern2 = Pattern.compile(PATTERN_2);
         Pattern pattern3 = Pattern.compile(PATTERN_3);
         Matcher matcher;
-        while (i<=138) {
-             matcher = pattern1.matcher(sc.nextLine());
+        while (i<6084) {
+            matcher = pattern1.matcher(sc.nextLine());
             if (matcher.find()) {
                 System.out.println(matcher.group());  // Выводит: подстрокой
             }
