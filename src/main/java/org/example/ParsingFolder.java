6245bb909a1702af728e51c8d422ced9cb7bc59f
67134 <gigalskiy@gmail.com>
2024-04-24 22:02:15 +0300

    next 22 24.04.2024

---
+++ src/main/java/org/example/ParsingFolder.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ParsingFolder {
+
+    public void parsingFolder(String fileName) throws FileNotFoundException {
+    File file = new File(fileName);
+    Scanner sc = new Scanner(file);
+
+    while(sc.hasNext()){
+
+        if (sc.next().)
+    }
+
+    }
+}
